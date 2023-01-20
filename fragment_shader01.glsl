ec330c77fe930c50aa9e4fedbfc8cd7120cde2a3
Randy Griffin <49879557+RandyGriffin@users.noreply.github.com>
2023-01-20 12:07:09 -0500

    Graphs created using C++ and CLion

---
+++ fragment_shader01.glsl
@@ -0,0 +1,42 @@
+#version 430 core
+
+out vec4 fColor;
+
+in vec4 wNormal;
+in vec4 wPosition;
+
+uniform layout(location = 4) vec4 ambient_light;
+uniform layout(location = 5) vec4 specular_light;
+uniform layout(location = 6) vec4 diffuse_light;
+uniform layout(location = 7) vec4 light_direction;
+uniform layout(location = 8) vec4 camera_position;
+
+
+uniform layout(location = 9) vec4 ambient_color;
+uniform layout(location = 10) vec4 specular_color;
+uniform layout(location = 11) vec4 diffuse_color;
+uniform layout(location = 12) float specular_shininess;
+uniform layout(location = 13) float specular_strength;
+
+void main()
+{
+    vec3 vnn = normalize(wNormal.xyz);
+    float diffuse_factor = max(0.0, dot(vnn, -normalize(light_direction.xyz)));
+
+    vec3 half_vector = normalize(normalize(-light_direction.xyz) - normalize(wPosition.xyz-camera_position.xyz));
+    float specular_factor = max(0.0, dot(vnn, half_vector));
+
+    if (diffuse_factor == 0.0)
+    specular_factor = 0.0;
+    else
+
+    specular_factor = pow(specular_factor, specular_shininess) * specular_strength;
+
+    vec4 ambient_component = ambient_color * ambient_light;
+    vec4 diffuse_component = diffuse_factor * diffuse_color * diffuse_light;
+    vec4 specular_component = specular_factor * specular_color * specular_light;
+
+    vec4 total = ambient_component + diffuse_component + specular_component;
+   // fColor = vec4(normalize(wNormal.xyz)/2 + .5, 1.0);
+    fColor = vec4(total.rgb, 1.0);
+}
